955d4113fc4f0aeb9ba746f95efe70b1e5d3174d
web_lion <794358525@qq.com>
2017-01-19 10:18:55 +0800

    header组件开发结束

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods">
+    我是goods
+  </div>
+</template>
+
+<script  type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
